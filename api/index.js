202bbe65b637044f2a5fba5677e3ce07498efad4
A4-AryanArshad <2021cs613@student.uet.edu.pk>
2025-06-12 12:57:49 +0500

    Added root route and updated for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -24,7 +24,9 @@ const cors = require('cors');
 
 app.use(cors());
 
-
+module.exports = (req, res) => {
+  res.status(200).json({ message: "Hello world from Vercel serverless function!" });
+};
 
 
 // Routes
